be80543789d353d5ca08c0bd125868a509d6b733
Siddharth <siddha.harth@gmail.com>
2017-08-24 20:01:41 +0530

    inital commit

---
+++ app/src/main/java/com/labs/svaithin/life_planner/MainActivity.java
@@ -0,0 +1,151 @@
+package com.labs.svaithin.life_planner;
+
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.labs.svaithin.life_planner.db.TaskContract;
+import com.labs.svaithin.life_planner.db.TaskDbHelper;
+
+import java.util.ArrayList;
+
+import static com.labs.svaithin.life_planner.R.id.lvItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private TaskDbHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onFabClick();
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+            }
+        });
+
+        //Custom Code start here
+
+        //DB handler
+        mHelper = new TaskDbHelper(this);
+
+
+        UpdateUI();
+
+    }
+
+    //Show UI List
+    void UpdateUI(){
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+
+        Cursor cursor = db.query(TaskContract.TaskEntry.PLAN,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.PLANNAME,
+                        TaskContract.TaskEntry.PCOMPLETED}, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.PLANNAME);
+            //mySimpleNewAdapter.add(cursor.getString(idx));
+            taskList.add(cursor.getString(idx));
+
+            //Log.d(TAG, "row" + doneMap);
+
+        }
+
+        //Code for testing
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        itemsAdapter.addAll(taskList);
+        //items.add("First Item");
+        //items.add("Second Item");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // This is the code for floating add button
+    public void onFabClick(){
+
+        final EditText taskEditText = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Add a Plan")
+                .setMessage("What do you want to do next?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                        SQLiteDatabase db = mHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(TaskContract.TaskEntry.PLANNAME, task);
+                        values.put(TaskContract.TaskEntry.PCOMPLETED, 0);
+                        values.put(TaskContract.TaskEntry.PARCHIVED, 0);
+                        db.insertWithOnConflict(TaskContract.TaskEntry.PLAN,
+                                null,
+                                values,
+                                SQLiteDatabase.CONFLICT_REPLACE);
+                        db.close();
+                        UpdateUI();
+
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/labs/svaithin/life_planner/db/TaskContract.java
@@ -0,0 +1,48 @@
+package com.labs.svaithin.life_planner.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Siddharth on 22/08/17.
+ */
+
+public class TaskContract {
+
+    public static final String DB_NAME = "com.svaithin.lifeplanner.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+
+        //Table names
+        public static final String PLAN = "plan";
+        public static final String GOAL = "goal";
+        public static final String HABIT = "habit";
+        public static final String NOTIFICATION = "notification";
+
+
+        //Colum name for PLAN
+        public static final String PLANNAME = "planname";
+        public static final String PCOMPLETED = "pcompleted";
+        public static final String PARCHIVED = "parch";
+
+        //Column name for GOAL
+        public static final String GOALNAME = "goalname";
+        public static final String GCOMPLETED = "gcompleted";
+        public static final String GARCHIVED = "garch";
+        public static final String PLANID = "planid";
+
+        // Column name for Habit
+        public static final String HABITNAME = "habitname";
+        public static final String HCOMPLETED = "hcompleted";
+        public static final String HARCHIVED = "harch";
+        public static final String GOALID = "goalid";
+
+        // Column name for Habit
+        public static final String NOTIFINAME= "notifname";
+        public static final String NCOMPLETED = "ncompleted";
+        public static final String NARCHIVED = "narch";
+        public static final String HABITID = "habitid";
+        public static final String TIME = "time";
+        public static final String DAYOFWEEK = "dayofweek";
+    }
+}
